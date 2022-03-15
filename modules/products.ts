8540badbe90d3d2fe01d85815a778d67aaf7d852
byhyakimaro <moderrevolution090217@gmail.com>
2022-03-15 03:27:54 -0300

    save

---
+++ modules/products.ts
@@ -0,0 +1,30 @@
+export const products: any = {
+  "expresso": {
+    "items" : [
+      {
+        "name": "Casquinha",
+        "img": "https://www.mrmixbrasil.com.br/arquivos-upload/categorias/sorvete-expresso-18012021170319812383.png"
+      },
+      {
+        "name": "Casquinha Recheada",
+        "img" : "https://bk-cms-dev.s3.amazonaws.com/_800x600_crop_center-center_none/Casquinha-Recheda-Ovomaltine-thumb_2021-09-16-161406_rtqu.png?mtime=20210916121407&focal=none&tmtime=20210916121530"
+      },
+      {
+        "name": "Cascao",
+        "img" : "https://www.mrmixbrasil.com.br/arquivos-upload/categorias/cascao-18012021170730707309.png"
+      },
+      {
+        "name": "Cascao Recheado",
+        "img" : "https://www.mrmixbrasil.com.br/arquivos-upload/categorias/cascao-trufado-21012021172215134119.png"
+      }
+    ]
+  },
+  "sundae": {
+    "items" : [
+      {
+        "name": "Sundae Tradicional",
+        "img": "https://www.mrmixbrasil.com.br/arquivos-upload/categorias/sorvete-sundae-18012021170330133303.png"
+      }
+    ]
+  }
+}
--- pages/produtos/[produto].tsx
+++ pages/produtos/[produto].tsx
@@ -1,36 +1,7 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
 import { Container } from '@styles/pages/home'
 
-const products: any = {
-  "expresso": {
-    "items" : [
-      {
-        "name": "Casquinha",
-        "img": "https://www.mrmixbrasil.com.br/arquivos-upload/categorias/sorvete-expresso-18012021170319812383.png"
-      },
-      {
-        "name": "Casquinha Recheada",
-        "img" : "https://bk-cms-dev.s3.amazonaws.com/_800x600_crop_center-center_none/Casquinha-Recheda-Ovomaltine-thumb_2021-09-16-161406_rtqu.png?mtime=20210916121407&focal=none&tmtime=20210916121530"
-      },
-      {
-        "name": "Cascao",
-        "img" : "https://www.mrmixbrasil.com.br/arquivos-upload/categorias/cascao-18012021170730707309.png"
-      },
-      {
-        "name": "Cascao Recheado",
-        "img" : "https://www.mrmixbrasil.com.br/arquivos-upload/categorias/cascao-trufado-21012021172215134119.png"
-      }
-    ]
-  },
-  "sundae": {
-    "items" : [
-      {
-        "name": "Sundae Tradicional",
-        "img": "https://www.mrmixbrasil.com.br/arquivos-upload/categorias/sorvete-sundae-18012021170330133303.png"
-      }
-    ]
-  }
-}
+import { products } from '../../modules/products'
 
 export default function Home({ items }:any) {
   
