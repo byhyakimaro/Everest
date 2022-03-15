2c9a8ea54293988310084e9fc763b2b5e83ba5cd
byhyakimaro <moderrevolution090217@gmail.com>
2022-03-15 04:48:35 -0300

    save

--- modules/items.ts
+++ modules/items.ts
@@ -4,5 +4,11 @@ export const items: any = [
     "name": "Casquinha",
     "price": 3.50,
     "img": "https://www.mrmixbrasil.com.br/arquivos-upload/categorias/sorvete-expresso-18012021170319812383.png"
+  },
+  { 
+    "code": 2,
+    "name": "Cascao",
+    "price": 6,
+    "img": "https://www.mrmixbrasil.com.br/arquivos-upload/categorias/cascao-18012021170730707309.png"
   }
 ]
