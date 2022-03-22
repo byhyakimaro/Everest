4f996a466329f0abdd427d00e51dc9eb9b8c0606
byhyakimaro <moderrevolution090217@gmail.com>
2022-03-22 13:26:59 -0300

    save

--- pages/markplace.tsx
+++ pages/markplace.tsx
@@ -4,9 +4,18 @@ import { Container } from '@styles/pages/home'
 import { Header } from 'components/header'
 import { useEffect } from 'react'
 
+import { items } from '../modules/items'
+
 export default function Home() {
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    const pedidos: any = JSON.parse(localStorage.getItem('pedidos') || '[]')
+    const n_pedidos: any = JSON.parse(localStorage.getItem('n_pedido') || '[]')
+    
+    const pedido = pedidos.find(({ id }:any) => id === n_pedidos)
+
+    console.log(items.find(({ code }:any) => code === pedido.items[0] ))
+  }, [])
 
   return(
   <>
